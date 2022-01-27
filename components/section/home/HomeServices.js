f561db11713820b57cfbef9bd7df0b11ef8afab4
sizar corpse <sizarcorpse@gmail.com>
2022-01-28 05:45:39 +0600

    created component homeServices

--- components/section/home/HomeServices.js
+++ components/section/home/HomeServices.js
@@ -1,30 +1,48 @@
 import {} from "react";
 import PropTypes from "prop-types";
 import { Box, styled, useTheme, useMediaQuery } from "@mui/material";
-import { MPIconBox, MPSlider } from "components/ui";
+import { MPIconBox, MPSlider, MPTitle } from "components/ui";
+
+const HomeServicesStyled = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: theme.spacing(10),
+  padding: theme.spacing(2),
+  [theme.breakpoints.down("md")]: {
+    gap: theme.spacing(6),
+  },
+  [theme.breakpoints.down("sm")]: {
+    gap: theme.spacing(1),
+  },
+  "& .contents": {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: theme.spacing(4),
+    maxWidth: "100%",
+  },
+}));
 
 const HomeServices = (props) => {
   const { data } = props;
   const matchesSM = useMediaQuery(useTheme().breakpoints.down("sm"));
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        gap: "40px",
-      }}
-    >
-      {matchesSM ? (
-        <MPSlider items={data.servicesList} mpComponent={<MPIconBox />} />
-      ) : (
-        data.servicesList.map((item, index) => (
-          <MPIconBox key={index} item={item} />
-        ))
-      )}
-    </Box>
+    <HomeServicesStyled>
+      <MPTitle title={data.title} subtitle={data.subtitle} alignment="center" />
+      <Box className="contents">
+        {matchesSM ? (
+          <MPSlider items={data.servicesList} mpComponent={<MPIconBox />} />
+        ) : (
+          data.servicesList.map((item, index) => (
+            <MPIconBox key={index} item={item} />
+          ))
+        )}
+      </Box>
+    </HomeServicesStyled>
   );
 };
 
