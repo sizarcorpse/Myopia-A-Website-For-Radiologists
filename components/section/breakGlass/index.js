a234268dcc2d9a2e6d195176e947f75521fad76e
sizar corpse <sizarcorpse@gmail.com>
2022-01-31 22:45:56 +0600

    refactoring

---
+++ components/section/breakGlass/index.js
@@ -0,0 +1,3 @@
+export { default as BreakGlassHero } from "./BreakGlassHero";
+export { default as BreakGlassIntroduction } from "./BreakGlassIntroduction";
+export { default as BreakGlassAccess } from "./BreakGlassAccess";
---
+++ components/section/onlineImage/index.js
@@ -0,0 +1,2 @@
+export { default as OnlineImageHero } from "./OnlineImageHero";
+export { default as OnlineImageFaq } from "./OnlineImageFaq";
